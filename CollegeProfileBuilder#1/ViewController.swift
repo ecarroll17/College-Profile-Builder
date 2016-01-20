8751db202ac55e7adfd899c67450b998a39e6840
ecarroll <student@ecarroll.local>
2016-01-20 11:23:41 -0600

    Connect Storyboard and added protocols

--- CollegeProfileBuilder#1/ViewController.swift
+++ CollegeProfileBuilder#1/ViewController.swift
@@ -2,14 +2,16 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
 {
-    @IBOutlet weak var myTableView: UITableView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var cities = ["Chicago", "Denver", "Seattle", "Miami", "Milwaukee"]
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
-        myTableView.dataSource = self
-        myTableView.delegate = self
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
 }
