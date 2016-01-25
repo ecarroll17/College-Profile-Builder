288dba14f969e56901ee97d5e261f91dec5a877b
ecarroll <student@ecarroll.local>
2016-01-25 11:26:14 -0600

    Allow for entries to be re-ordered

--- CollegeProfileBuilder#1/ViewController.swift
+++ CollegeProfileBuilder#1/ViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
 {
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var editButton: UIBarButtonItem!
     
     var cities = ["Chicago", "Denver", "Seattle", "Miami", "Milwaukee"]
 
@@ -12,6 +13,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         tableView.dataSource = self
         tableView.delegate = self
+        editButton.tag = 0
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
@@ -34,6 +36,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             tableView.reloadData()
         }
     }
+    
     @IBAction func onTappedPlusButton(sender: UIBarButtonItem)
     {
         let alert = UIAlertController(title: "Add City", message: nil, preferredStyle: .Alert)
@@ -56,5 +59,30 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         self.presentViewController(alert, animated: true, completion: nil)
     }
+    
+    func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool
+    {
+        return true
+    }
+    
+    func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath)
+    {
+        let city = cities[sourceIndexPath.row]
+        cities.removeAtIndex(sourceIndexPath.row)
+        cities.insert(city, atIndex: destinationIndexPath.row)
+    }
+    
+    @IBAction func onTappedEditButton(sender: UIBarButtonItem)
+    {
+        if sender.tag == 0 {
+            tableView.editing = true
+            sender.tag = 1
+        }
+        else {
+            tableView.editing = false
+            sender.tag = 0
+        }
+    }
+
 }
 
